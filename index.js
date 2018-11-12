bc1621597e0fae7523925235867adcff0f2e01d6
Cory Kim <github@cory.kim>
2018-11-12 17:07:47 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const q = require('daskeyboard-applet');
+const logger = q.logger;
+
+const request = require('request-promise');
+
+const apiUrl = 'https://api.trello.com/1';
+
+const keyLastActionDate = 'lastActionDate';
+
+async function getBoards(apiKey, token) {
+  return request.get({
+    url: apiUrl + `/members/me/boards?key=${apiKey}&token=${token}`,
+    json: true
+  }).catch(error => {
+    logger.error("Could not get boards: " + error);
+    throw error;
+  });
+}
+
+async function getActionsForBoard(boardId, apiKey, token) {
+  return request.get({
+    url: apiUrl + `/boards/${boardId}/actions?key=${apiKey}&token=${token}`,
+    json: true
+  }).catch(error => {
+    logger.error(`Could not get actions for board ${boardId}: ` + error);
+    throw error;
+  });
+}
+
+class Trello extends q.DesktopApp {
+  async run() {
+    console.log("Running.");
+    return this.getNewActions().then(newActions => {
+      if (newActions && newActions.length > 0) {
+        logger.info("Got " + newActions.length + " new actions.");
+        return new q.Signal([
+          [new q.Point("#00FF00")]
+        ]);
+      } else {
+        return null;
+      }
+    })
+  }
+
+  async getNewActions() {
+    const newActions = [];
+    const lastActionDate = this.store.get(keyLastActionDate) ||
+      '';
+    let newestActionDate = null;
+
+    logger.info("Checking for actions since: " + lastActionDate);
+
+    return getBoards(this.authorization.apiKey,
+      this.authorization.token).then(async (boards) => {
+      if (boards && boards.length > 0) {
+        for (let board of boards) {
+          const actions = await getActionsForBoard(board.id,
+            this.authorization.apiKey, this.authorization.token);
+          for (let action of actions) {
+            if (action.date > lastActionDate) {
+              logger.info(
+                `Found new action date ${action.date} (vs: ${lastActionDate})`);
+              if (action.date > newestActionDate) {
+                newestActionDate = action.date;
+              }
+              newActions.push(action);
+            }
+          }
+        }
+      }
+
+      if (newestActionDate != null) {
+        this.store.put(keyLastActionDate, newestActionDate);
+      }
+
+      return newActions;
+    }).catch(error => {
+      throw error;
+    });
+  }
+
+}
+
+
+module.exports = {
+  getBoards: getBoards,
+  getActionsForBoard: getActionsForBoard,
+  keyLastActionDate: keyLastActionDate,
+  Trello: Trello
+}
+
+const applet = new Trello();
---
+++ test/test.js
@@ -0,0 +1,100 @@
+const assert = require('assert');
+const t = require('../index');
+const auth = require('./auth.json');
+
+describe('getBoards', function () {
+  it('can get boards', function () {
+    return t.getBoards(auth.apiKey, auth.token).then((boards) => {
+      console.log("Boards: ", boards);
+      assert.ok(boards);
+      assert.ok(boards[0]);
+      assert.ok(boards[0].id);
+    }).catch((error) => {
+      assert.fail(error);
+    })
+  })
+});
+
+describe('getActionsForBoard', function () {
+  it('can get actions for board', function () {
+    return t.getActionsForBoard('5be9f7545b05e45e0092f4dd',
+      auth.apiKey, auth.token).then(actions => {
+      console.log('Actions: ', actions);
+      assert.ok(actions);
+      assert.ok(actions[0]);
+      assert.ok(actions[0].id);
+    }).catch(error => {
+      assert.fail(error);
+    })
+  })
+});
+
+describe('Trello', () => {
+  async function makeApp() {
+    let app = new t.Trello();
+
+    await app.processConfig({
+      extensionId: 777,
+      geometry: {
+        width: 1,
+        height: 1,
+      },
+      authorization: auth,
+      applet: {}
+    });
+
+    return app;
+  }
+
+  describe('#getNewActions', () => {
+    it('can get new actions with null lastActionDate', function () {
+      return makeApp().then(app => {
+        app.store.put(t.keyLastActionDate, null);
+        return app.getNewActions().then(actions => {
+          assert.ok(actions);
+          assert.ok(actions[0]);
+          assert.ok(actions[0].id);
+        }).catch(error => {
+          assert.fail(error);
+        })
+      })
+    });
+
+    it("gets new actions with old lastActionDate", function () {
+      return makeApp().then(app => {
+        app.store.put(t.keyLastActionDate, 
+          new Date('2018-01-17T03:24:00').toISOString());
+        return app.getNewActions().then(actions => {
+          assert.ok(actions);
+          assert.ok(actions[0]);
+          assert.ok(actions[0].id);
+        }).catch(error => {
+          assert.fail(error);
+        })
+      })
+    });
+
+    it("doesn't return new actions with current lastActionDate", function () {
+      return makeApp().then(app => {
+        app.store.put(t.keyLastActionDate, new Date().toISOString);
+        return app.getNewActions().then(actions => {
+          assert.ok(actions);
+          assert(actions.length === 0);
+        }).catch(error => {
+          assert.fail(error);
+        })
+      })
+    });
+  });
+
+  describe('#run()', () => {
+    return makeApp().then(app => {
+      return app.run().then((signal) => {
+        console.log(signal);
+        assert.ok(signal);
+      }).catch((error) => {
+        assert.fail(error)
+      });
+    })
+  })
+})
